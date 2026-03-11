735699793befd81cb3e86be663c2f70479a03b37
Ruslan <ruslan@compressto20kb.com>
2026-03-11 15:42:48 +0200

    perf: compress+WebP/AVIF images

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compress: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 31536000,
+  },
   experimental: {
     optimizeCss: true,
   },
 };
-
 module.exports = nextConfig;
