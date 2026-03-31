a42791a2e8d67ee39b9b4645237d541c89e1038c
Ruslan <ruslan@compressto20kb.com>
2026-03-31 16:47:47 +0300

    seo: allow /_next/ for Googlebot (critical for JS rendering)

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/api/', '/_next/'],
+        disallow: ['/api/'],
       },
     ],
     sitemap: 'https://www.compressto20kb.com/sitemap.xml',
