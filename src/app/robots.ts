e3409ae3cd16ca16a448965c2fdcf275b4a85975
Ruslan <ruslan@compressto20kb.com>
2026-05-14 11:06:24 +0300

    fix: robots.ts www sitemap+host + block JS files

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,15 +1,14 @@
 import { MetadataRoute } from 'next';
-
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/api/', '/compress-for-[country]/']
+        disallow: ['/api/', '/compress-for-[country]/', '/*.js$', '/*?*'],
       },
     ],
-    sitemap: 'https://compressto20kb.com/sitemap.xml',
-    host: 'https://compressto20kb.com',
+    sitemap: 'https://www.compressto20kb.com/sitemap.xml',
+    host: 'https://www.compressto20kb.com',
   };
 }
